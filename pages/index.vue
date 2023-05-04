f0f3e5f1a8fcdb904b73fdf2c4f991c54420ce57
Iroha71 <y.koba.h1071@outlook.jp>
2023-05-04 15:20:22 +0900

    [update] ランディングページに表示されるカードコンポーネント追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <VBtn>ボタン</VBtn>
+  <div class="d-flex justify-left">
+    <IndexCard
+      v-for="category in $categories()"
+      :key="category.title"
+      :title="category.title"
+      :subtitle="category.descShort"
+      :text="category.text"
+      :path="category.contentPath" />
   </div>
 </template>
