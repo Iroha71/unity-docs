204daec54317d08f688e6d0644edb963a2d857c5
Iroha71 <42258351+Iroha71@users.noreply.github.com>
2023-05-04 22:20:23 +0900

    Feature/#8 ナビゲーション策定 (#9)
    * [refactor] NavDrawerをコンポーネント化
    
    * [update] バーの色とデザイン変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,10 @@
 <template>
   <v-layout>
-    <VAppBar color="surface-variant" title="Unity Docs" />
-    <VNavigationDrawer color="grey-darken-2" floating permanent >
-      <VList nav>
-        <VListItem title="inbox" value="inbox" prepend-icon="mdi-email" @click="$router.push({path: '/'})"></VListItem>
-      </VList>
-    </VNavigationDrawer>
+    <VAppBar color="#19A7CE" title="Unity 古エッダ" >
+      <VSpacer />
+      <p>ver.1.0.0</p>
+    </VAppBar>
+    <SideNavbar />
     <VMain>
       <div class="container">
         <NuxtPage />
@@ -21,5 +20,8 @@ const router = useRouter()
 <style>
 .container {
   padding: 1rem 1rem;
+}
+p {
+  margin-right: 1rem;
 }
 </style>
