d8eb67d0b6030434ab2d8acbd50fc1d6c8d26425
Iroha71 <y.koba.h1071@outlook.jp>
2023-06-27 21:56:13 +0900

    [add] ダメ―計算式

--- plugins/constants.ts
+++ plugins/constants.ts
@@ -1,6 +1,7 @@
 export default defineNuxtPlugin (nuxtApp => {
   const categories = [
     { title: "Invector", contentPath: "/invector", descShort: "TPSコントローラ", text: "" },
+    { title: "ゲームデザイン関連", contentPath: "/game_design", descShort: "ゲームデザイン", text: "" },
   ]
   return {
     provide: {
