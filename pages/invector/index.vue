15786cf6670e2fdd48d9a16a8ef188c7bcf4e305
Iroha71 <42258351+Iroha71@users.noreply.github.com>
2023-06-29 00:02:20 +0900

    Feature/#17 change invec dir (#21)
    * [refactor] 階層構造を変更
    
    * [fix] マージン等を修正
    
    * [fix] カードのマージン追加

--- pages/invector/index.vue
+++ pages/invector/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <ContentList path="/invector" v-slot="{ list }">
-      <IndexCard
-        v-for="article in list" :key="article._path"
-        :title="article.title"
-        :subtitle="article.description"
-        :path="article._path" />
-    </ContentList>
+    <ContentDoc />
   </div>
 </template>
