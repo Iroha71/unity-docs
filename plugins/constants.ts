227119e91e3643e1d672688c32134fa02dde2482
Iroha71 <42258351+Iroha71@users.noreply.github.com>
2023-06-29 09:31:24 +0900

    Feature/#11 add quest machine (#23)
    * [add] Quest Machine
    
    * [update] QuestMachine情報追加

--- plugins/constants.ts
+++ plugins/constants.ts
@@ -2,6 +2,7 @@ export default defineNuxtPlugin (nuxtApp => {
   const categories = [
     { title: "Invector", contentPath: "/invector", descShort: "TPSコントローラ", text: "" },
     { title: "ゲームデザイン関連", contentPath: "/game_design", descShort: "ゲームデザイン", text: "" },
+    { title: "Quest Machine", contentPath: "/questmachine", descShort: "クエスト管理アセット", text: ""},
   ]
   return {
     provide: {
