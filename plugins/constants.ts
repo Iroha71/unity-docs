5eb36dc834301cd0a8e3f2b7a5db9d3a70687b2a
Iroha71 <y.koba.h1071@outlook.jp>
2023-06-29 19:07:47 +0900

    [add] シーン追加時の処理

--- plugins/constants.ts
+++ plugins/constants.ts
@@ -1,6 +1,7 @@
 export default defineNuxtPlugin (nuxtApp => {
   const categories = [
     { title: "Invector", contentPath: "/invector", descShort: "TPSコントローラ", text: "" },
+    { title: "シーンセットアップ", contentPath: "/scene_setup", descShort: "セットアップ手順", text: ""},
     { title: "ゲームデザイン関連", contentPath: "/game_design", descShort: "ゲームデザイン", text: "" },
     { title: "Quest Machine", contentPath: "/questmachine", descShort: "クエスト管理アセット", text: ""},
   ]
