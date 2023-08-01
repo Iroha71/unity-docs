e6c9c2293ea0ee1d8b8c78b44879f011bcc66d86
Iroha71 <y.koba.h1071@outlook.jp>
2023-08-01 16:08:05 +0900

    [update] gaia / enviro追加

---
+++ content/gaia/index.md
@@ -0,0 +1,18 @@
+# GAIA
+
+## 草原の表現
+
+- global spawn density: 0.8
+- ProtoType / Prefab
+  - width min max: 1.5
+  - height min max: 1.5
+  - noise spread: 3
+- Spawing
+  - Detail Density: 40
+  - Min Fitness %: 50
+  - Fadeout Fitness %: 60
+
+## スタンプ
+
+- 初期の大きさ
+  - x: 100 y: 5 z:100
--- plugins/constants.ts
+++ plugins/constants.ts
@@ -5,6 +5,8 @@ export default defineNuxtPlugin (nuxtApp => {
     { title: "ゲームデザイン関連", contentPath: "/game_design", descShort: "ゲームデザイン", text: "" },
     { title: "Quest Machine", contentPath: "/questmachine", descShort: "クエスト管理アセット", text: ""},
     { title: "Dialogue System", contentPath: "/dialoguesystem", descShort: "会話アセット", text: ""},
+    { title: "Enviro", contentPath: "/enviro", descShort: "天候管理アセット", text: ""},
+    { title: "Gaia2", contentPath: "/gaia", descShort: "地形編集アセット", text: "" },
   ]
   return {
     provide: {
