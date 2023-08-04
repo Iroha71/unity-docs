a16313a52f1a41cadf4b6c48f26a36b8d1f2d568
Iroha71 <y.koba.h1071@outlook.jp>
2023-08-04 17:07:55 +0900

    [update] トップページのレイアウトを修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="d-flex justify-left">
-    <IndexCard
-      v-for="category in $categories()"
-      :key="category.title"
-      :title="category.title"
-      :subtitle="category.descShort"
-      :text="category.text"
-      :path="category.contentPath" />
-  </div>
+  <v-container>
+    <v-row no-gutters>
+      <IndexCard
+        v-for="category in $categories()"
+        :key="category.title"
+        :title="category.title"
+        :subtitle="category.descShort"
+        :text="category.text"
+        :path="category.contentPath" />
+    </v-row>
+  </v-container>
 </template>
 
 <style lang="scss">
