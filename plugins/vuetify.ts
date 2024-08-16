3dce253831ac2184b476314dd53398d92cff3204
iroha <y.koba.h1071@outlook.jp>
2024-08-16 09:14:29 +0900

    material design iconで表示されないものを修正

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,16 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin(app => {
   const vuetify = createVuetify({
     ssr: false,
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi',
+    }
   })
 
   app.vueApp.use(vuetify)
