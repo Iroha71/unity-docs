c812940253c36a26e848f41263b66e7dd021ee77
iroha <y.koba.h1071@outlook.jp>
2024-10-05 22:58:58 +0900

    記事をカテゴリに分類して表示するように変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,30 @@
 <template>
-  <v-container>
-    <v-row no-gutters>
-      <IndexCard
-        v-for="category in $categories()"
-        :key="category.title"
-        :title="category.title"
-        :subtitle="category.descShort"
-        :text="category.text"
-        :content-name="category.contentName"
-        :color-variant="category.colorVariant"
-        :icon="category.prependIcon"
-      />
-    </v-row>
-  </v-container>
+  <v-container fluid>
+    <VCard v-for="category in $categories()" 
+      :key="category.name"
+      variant="outlined"
+      color="grey-darken-1"
+      >
+      <VCardItem>
+        <div class="text-overline mb-1">{{ category.name }}</div>
+      </VCardItem>
+      <VContainer fluid>
+        <v-row >
+          <VCol md="3" sm="6" xs="12" v-for="content in category.contents" :key="content.title">
+            <IndexCard 
+              :key="content.title"
+              :title="content.title"
+              :subtitle="content.description"
+              :text="content.text"
+              :content-name="content.contentName"
+              :color-variant="content.colorVariant"
+              :icon="content.prependIcon"
+            />
+          </VCol>
+        </v-row>
+      </VContainer>
+    </VCard>
+  </v-container>  
 </template>
 
 <style lang="scss">
--- plugins/constants.ts
+++ plugins/constants.ts
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin (nuxtApp => {
 
-  interface category {
+  interface content {
     title: string,
     contentName: string,
     descShort: string,
@@ -9,103 +9,139 @@ export default defineNuxtPlugin (nuxtApp => {
     colorVariant: string,
   }
 
+  interface category {
+    name: string,
+    contents: content[],
+  }
+
   const categories: category[] = [
     {
-      title: "ゲームデザイン関連",
-      contentName: "game_design",
-      descShort: "ゲームデザイン",
-      text: "ダメージ計算式など",
-      prependIcon: "mdi-palette",
-      colorVariant: "purple-lighten-1",
+      name: 'コントローラ',
+      
+      contents: [
+        {
+          title: "Invector",
+          contentName: "invector",
+          descShort: "TPSコントローラ",
+          text: "拡張に必要な情報",
+          prependIcon: "mdi-run",
+          colorVariant: "grey-darken-1",
+        },
+        {
+          title: "Horse Animset",
+          contentName: "hap",
+          descShort: "馬コントローラ",
+          text: "セットアップ時の補足情報",
+          prependIcon: "mdi-horse",
+          colorVariant: "amber-darken-1",
+        },
+      ],
     },
     {
-      title: "シーンセットアップ",
-      contentName: "scene_setup",
-      descShort: "セットアップ手順",
-      text: "初期配置オブジェクトについて",
-      prependIcon: "mdi-tune",
-      colorVariant: "indigo-darken-1",
+      name: 'セットアップと自作',
+      contents: [
+        {
+          title: "シーンセットアップ",
+          contentName: "scene_setup",
+          descShort: "セットアップ手順",
+          text: "初期配置オブジェクトについて",
+          prependIcon: "mdi-tune",
+          colorVariant: "indigo-darken-1",
+        },
+        {
+          title: "ゲームデザイン関連",
+          contentName: "game_design",
+          descShort: "ゲームデザイン",
+          text: "ダメージ計算式など",
+          prependIcon: "mdi-palette",
+          colorVariant: "purple-lighten-1",
+        },
+        {
+          title: "自作スクリプト",
+          contentName: "original",
+          descShort: "自作スクリプト",
+          text: "戦闘拡張、他の工夫など",
+          prependIcon: "mdi-account",
+          colorVariant: "brown-lighten-1",
+        },
+        {
+          title: "役立つTips",
+          contentName: "useful_tips",
+          descShort: "役立つ情報を記載",
+          text: "役立つ情報を記載",
+          prependIcon: "mdi-lightbulb-on-10",
+          colorVariant: "red-darken-1",
+        }
+      ],
     },
     {
-      title: "自作スクリプト",
-      contentName: "original",
-      descShort: "自作スクリプト",
-      text: "戦闘拡張、他の工夫など",
-      prependIcon: "mdi-account",
-      colorVariant: "brown-lighten-1",
+      name: 'RPG',
+      contents: [
+        {
+          title: "Dialogue System",
+          contentName: "dialoguesystem",
+          descShort: "会話アセット",
+          text: "セットアップ方法など",
+          prependIcon: "mdi-forum-outline",
+          colorVariant: "cyan-darken-1",
+        },
+        {
+          title: "Quest Machine",
+          contentName: "questmachine",
+          descShort: "クエスト管理アセット",
+          text: "セーブ方法やイベント通知実装",
+          prependIcon: "mdi-book-variant",
+          colorVariant: "amber-darken-4",
+        },
+      ]
     },
     {
-      title: "Dialogue System",
-      contentName: "dialoguesystem",
-      descShort: "会話アセット",
-      text: "セットアップ方法など",
-      prependIcon: "mdi-forum-outline",
-      colorVariant: "cyan-darken-1",
+      name: '地形',
+      contents: [
+        {
+          title: "Enviro",
+          contentName: "enviro",
+          descShort: "天候管理アセット",
+          text: "タイムラプス等の実装",
+          prependIcon: "mdi-cloud",
+          colorVariant: "light-green-darken-1",
+        },
+        {
+          title: "Gaia2",
+          contentName: "gaia",
+          descShort: "地形編集アセット",
+          text: "うまく地形を作るためのTIPS",
+          prependIcon: "mdi-image-filter-hdr",
+          colorVariant: "green-lighten-1",
+        },
+      ]
     },
     {
-      title: "Enviro",
-      contentName: "enviro",
-      descShort: "天候管理アセット",
-      text: "タイムラプス等の実装",
-      prependIcon: "mdi-cloud",
-      colorVariant: "light-green-darken-1",
+      name: 'エフェクト',
+      contents: [
+        {
+          title: "X-Weapon Trail",
+          contentName: "xweapon_trail",
+          descShort: "武器追従エフェクト",
+          text: "セットアップ方法",
+          prependIcon: "mdi-sword",
+          colorVariant: "light-blue-darken-4",
+        },
+      ]
     },
     {
-      title: "Gaia2",
-      contentName: "gaia",
-      descShort: "地形編集アセット",
-      text: "うまく地形を作るためのTIPS",
-      prependIcon: "mdi-image-filter-hdr",
-      colorVariant: "green-lighten-1",
+      name: 'UI',
+      contents: [
+        {
+          title: "Ultimate Clean GUI",
+          contentName: "ultimate_clean_gui",
+          descShort: "GUIの通知等を記載",
+          text: "GUIの通知の実装",
+          prependIcon: "mdi-animation",
+          colorVariant: "deep-purple-darken-1",
+        },
+      ]
     },
-    {
-      title: "Horse Animset",
-      contentName: "hap",
-      descShort: "馬コントローラ",
-      text: "セットアップ時の補足情報",
-      prependIcon: "mdi-horse",
-      colorVariant: "amber-darken-1",
-    },
-    {
-      title: "Invector",
-      contentName: "invector",
-      descShort: "TPSコントローラ",
-      text: "拡張に必要な情報",
-      prependIcon: "mdi-run",
-      colorVariant: "grey-darken-1",
-    },
-    {
-      title: "Quest Machine",
-      contentName: "questmachine",
-      descShort: "クエスト管理アセット",
-      text: "セーブ方法やイベント通知実装",
-      prependIcon: "mdi-book-variant",
-      colorVariant: "amber-darken-4",
-    },
-    {
-      title: "Ultimate Clean GUI",
-      contentName: "ultimate_clean_gui",
-      descShort: "GUIの通知等を記載",
-      text: "GUIの通知の実装",
-      prependIcon: "mdi-animation",
-      colorVariant: "deep-purple-darken-1",
-    },
-    {
-      title: "X-Weapon Trail",
-      contentName: "xweapon_trail",
-      descShort: "武器追従エフェクト",
-      text: "セットアップ方法",
-      prependIcon: "mdi-sword",
-      colorVariant: "light-blue-darken-4",
-    },
-    {
-      title: "役立つTips",
-      contentName: "useful_tips",
-      descShort: "役立つ情報を記載",
-      text: "役立つ情報を記載",
-      prependIcon: "mdi-lightbulb-on-10",
-      colorVariant: "red-darken-1",
-    }
   ];
   return {
     provide: {
