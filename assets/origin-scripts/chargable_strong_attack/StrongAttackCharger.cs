942114786a461785041cb3f359d9cb2a546b30df
iroha <y.koba.h1071@outlook.jp>
2024-10-28 23:13:47 +0900

    強攻撃をチャージ式に変更する方法を追加

---
+++ assets/origin-scripts/chargable_strong_attack/StrongAttackCharger.cs
@@ -0,0 +1,122 @@
+﻿using Invector.vCharacterController;
+using Invector.vMelee;
+using UnityEngine;
+using UnityEngine.Events;
+
+[DefaultExecutionOrder(5)]
+public class StrongAttackCharger : MonoBehaviour
+{
+    [SerializeField]
+    private vShooterMeleeInput tpInput;
+    [SerializeField]
+    private vMeleeManager meleeManager;
+    private int currentLevel = 0;
+    private const int MAX_CHARGE_LEVEL = 2;
+    private const float CHARGE_COMPLETE_TIME = 2f;
+    private float charge = 0f;
+    public UnityAction<float> OnChangedValue;
+    public UnityAction<int> OnChangedChargeLevel;
+    private const int CHARGE_PER_LEVEL = 100;
+    [Header("音響"), SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip chargeSound;
+    [SerializeField]
+    private AudioClip filledSound;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tpInput.cc.OnRoll.AddListener(ResetCharge);
+        //meleeManager.onDamageHit.AddListener((hitinfo) => AddCharge(5f));
+        tpInput.OnPressingStrongAttack += AddCharge;
+        tpInput.OnCanceledStrongAttack += ResetCharge;
+        tpInput.OnReleasedStrongAttack += PlayStrongAttack;
+    }
+
+    /// <summary>
+    /// チャージ量を加算する
+    /// </summary>
+    public void AddCharge()
+    {
+        // チャージ開始時
+        if (charge <= 0f)
+        {
+            // 音源はループさせる
+            AudioSource _src = Instantiate(audioSource, transform);
+            _src.transform.localPosition = Vector3.zero;
+            _src.PlayOneShot(chargeSound);
+        }
+
+        // 1レベル溜めるまでに何秒かかるか（CHARGE_COMPLETE_TIME）をもとに加算量を計算
+        float addValue = 100f / CHARGE_COMPLETE_TIME * Time.deltaTime;
+        charge = Mathf.Clamp(addValue + charge, 0f, CHARGE_PER_LEVEL * MAX_CHARGE_LEVEL);
+        OnChangedValue?.Invoke(charge);
+
+        if (CanLevelUp())
+        {
+            AddLevel();
+        }
+    }
+
+    /// <summary>
+    /// チャージレベルを上昇できるか
+    /// </summary>
+    /// <returns>true＝レベル上昇可能</returns>
+    private bool CanLevelUp()
+    {
+        int level = (int)charge / 100;
+        return level > currentLevel;
+    }
+
+    /// <summary>
+    /// チャージ量に応じた強攻撃を実行する
+    /// </summary>
+    public void PlayStrongAttack()
+    {
+        if (charge >= 100f)
+        {
+            tpInput.animator.SetInteger("ActionState", -1);
+            tpInput.TriggerStrongAttack();
+        }
+        else
+        {
+            tpInput.TriggerStrongAttack();
+        }
+
+        ResetCharge();
+        ResetLevel();
+    }
+
+    /// <summary>
+    /// チャージレベルを上昇させる
+    /// </summary>
+    public void AddLevel()
+    {
+        if (currentLevel >= MAX_CHARGE_LEVEL)
+            return;
+
+        AudioSource _audioSrc = Instantiate(audioSource, transform.position, Quaternion.identity);
+        _audioSrc.PlayOneShot(filledSound);
+        currentLevel++;
+        OnChangedChargeLevel?.Invoke(currentLevel);
+    }
+
+    /// <summary>
+    /// チャージレベルを0にする
+    /// </summary>
+    public void ResetLevel()
+    {
+        currentLevel = 0;
+        OnChangedChargeLevel?.Invoke(0);
+    }
+
+    /// <summary>
+    /// チャージ量を0にする
+    /// </summary>
+    public void ResetCharge()
+    {
+        charge = 0f;
+        OnChangedValue?.Invoke(charge);
+    }
+}
--- content/invector/melee.md
+++ content/invector/melee.md
@@ -14,6 +14,8 @@
   - [Death By AnimationWithRagdollのタイミング調整](#death-by-animationwithragdollのタイミング調整)
   - [スタン状態の実装](#スタン状態の実装)
   - [テイクダウン](#テイクダウン)
+  - [強攻撃をチャージタイプにする](#強攻撃をチャージタイプにする)
+    - [使用例](#使用例)
   - [長押し・単押し出し分け](#長押し単押し出し分け)
   - [ボタン同時押し](#ボタン同時押し)
   - [追加ダメージ設定](#追加ダメージ設定)
@@ -349,6 +351,102 @@
 9. （任意）ステルスの場合
    1. AI側のDetection > MinDistanceを0にする
       1. 背後に接近時プレイヤーを検知しないようにするため
+
+## 強攻撃をチャージタイプにする
+
+- vMeleeCombatInput.csにプロパティを追加する
+
+    ``` csharp
+    // チャージ中か
+    protected bool isChargingStrongAttack = false;
+    // 強攻撃ボタン長押し中のコールバック
+    public UnityAction OnPressingStrongAttack;
+    // 強攻撃ボタンを離したコールバック
+    public UnityAction OnReleasedStrongAttack;
+    // 強攻撃がキャンセルされた際のコールバック
+    public UnityAction OnCanceledStrongAttack;
+    ```
+
+- vMeleeCombatInput.cs > MeleeStrongAttackInput()を編集する
+
+    ``` csharp
+    public virtual void MeleeStrongAttackInput()
+    {
+        if (animator == null)
+        {
+            return;
+        }
+
+        // 元のコードを削除
+        //if (strongAttackInput.GetButtonDown() && (!meleeManager.CurrentActiveAttackWeapon || meleeManager.CurrentActiveAttackWeapon.useStrongAttack) && MeleeAttackStaminaConditions())
+        //{
+        //    TriggerStrongAttack();
+        //}
+        
+        // 強攻撃可能な武器か判定
+        if (meleeManager.CurrentActiveAttackWeapon == null || meleeManager.CurrentActiveAttackWeapon.useStrongAttack == false)
+            return;
+        // 強攻撃長押し
+        if (strongAttackInput.GetButton()
+            && MeleeAttackStaminaConditions())
+        {
+            isChargingStrongAttack = true;
+            OnPressingStrongAttack?.Invoke();
+        }
+        // 強攻撃ボタンを離した際
+        else if (isChargingStrongAttack)
+        {
+            if (meleeManager.CurrentActiveAttackWeapon.useStrongAttack)
+            {
+                OnReleasedStrongAttack?.Invoke();
+            }
+            else
+            {
+                OnCanceledStrongAttack?.Invoke();
+            }
+            isChargingStrongAttack = false;
+        }
+    }
+    ```
+
+- MeleeStrongAttackInput()の呼び出し元の編集
+    - Shooter = vShooterMeleeInput > InputHandle()
+    - Melee = vMeleeCombatInput > InputHandle()
+
+    ``` csharp
+    #region MeleeInput
+
+    if (MeleeAttackConditions() && !IsAiming && !isReloading && !lockMeleeInput && !CurrentActiveWeapon)
+    {
+        // チャージ中は弱攻撃ができないように
+        if ((shooterManager.canUseMeleeWeakAttack_H || shooterManager.CurrentWeapon == null)
+            && isChargingStrongAttack == false)
+        {
+            MeleeWeakAttackInput();
+        }
+
+        if (shooterManager.canUseMeleeStrongAttack_H || shooterManager.CurrentWeapon == null)
+        {
+            MeleeStrongAttackInput();
+        }
+        // チャージ中はガードができないように
+        if ((shooterManager.canUseMeleeBlock_H || shooterManager.CurrentWeapon == null)
+            && isChargingStrongAttack == false)
+        {
+            BlockingInput();
+        }
+        else
+        {
+            isBlocking = false;
+        }
+    }
+
+    #endregion
+    ```
+
+### 使用例
+
+- [ここのスクリプトを参照](https://github.com/Iroha71/unity-docs/blob/develop/assets/origin-scripts/chargable_strong_attack/)
 
 ## 長押し・単押し出し分け
 
