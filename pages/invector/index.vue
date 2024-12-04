c5533ce8cf6da271c99e854ef9acafc29294e345
iroha <y.koba.h1071@outlook.jp>
2024-12-04 18:26:04 +0900

    invectorページのレイアウトを記事一覧へ変更

--- pages/invector/index.vue
+++ pages/invector/index.vue
@@ -1,5 +1,75 @@
 <template>
-  <div>
-    <ContentDoc />
-  </div>
+  <VContainer fluid>
+    <VRow 
+      dense
+      v-for="artGroup in articleGroup"
+      :key="artGroup.category"
+    >
+      <VCol
+        cols="3"
+        v-for="article in artGroup.articles"
+        :key="article.pageName"
+      >
+        <ArticleLinkCard
+          :title="article.title"
+          :icon="article.icon"
+          :page-name="`invector/${article.pageName}`"
+          :category="artGroup.category"
+          :color="getCardColor(artGroup.category)"
+          :description="article.addInfo"
+        />
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
+
+<script setup lang="ts">
+import ArticleLinkCard from '~/components/ArticleLinkCard.vue';
+
+const getCardColor = (category: string) => {
+  switch (category) {
+    case "melee":
+      return "red-lighten-1";
+    case "shooter":
+      return "indigo-lighten-1";
+    case "item manager":
+      return "green-lighten-1";
+    case "ai":
+      return "orange-lighten-1";
+    default:
+      return "transparent";
+  }
+}
+
+interface ArticleGroup {
+  category: string,
+  articles: ArticleConfig[],
+}
+
+interface ArticleConfig {
+  title: string,
+  icon: string,
+  pageName: string,
+  addInfo: string,
+}
+
+const meleeArticles = reactive<ArticleConfig[]>([
+  { title: "パリィやガードブレイクしたい", icon: "shield-off-outline", pageName: "guardbreak", addInfo: "ガードブレイク・パリィ" },
+  { title: "ローリングキャンセルについて", icon: "rotate-left", pageName: "rolling_cancell", addInfo: "実装方法と硬直の解消などの改善" },
+  { title: "スタンさせたい", icon: "creation-outline", pageName: "stun", addInfo: "実装方法" },
+  { title: "テイクダウンしたい", icon: "knife-military", pageName: "takedown", addInfo: "実装方法" },
+  { title: "長押し処理のまとめ", icon: "controller", pageName: "button_longpress", addInfo: "チャージ攻撃・だし分け" },
+  { title: "ダメージに要素を追加したい", icon: "sword-cross", pageName: "damage_config", addInfo: "要素の追加・無敵化" },
+  { title: "左手でも攻撃したい", icon: "hand-back-left-outline", pageName: "lefthand_attack", addInfo: "武器装備や入力について" },
+  { title: "状態異常を追加したい", icon: "bottle-tonic-skull", pageName: "abnormal", addInfo: "状態異常ロジックの提示" }
+]);
+
+const shooterArticles = reactive<ArticleConfig[]>([
+  { title: "Shooter: セットアップ", icon: "pistol", pageName: "shooter_setup", addInfo: "" }
+]);
+
+const articleGroup = reactive<ArticleGroup[]>([
+  { category: "melee", articles: meleeArticles },
+  { category: "shooter", articles: shooterArticles },
+]);
+</script>
