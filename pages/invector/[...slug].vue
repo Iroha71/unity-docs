090a41caf843392c4416f2857275e9178b5ddeb9
iroha <y.koba.h1071@outlook.jp>
2025-02-16 22:02:29 +0900

    記事一覧へ戻るボタンを追加

--- pages/invector/[...slug].vue
+++ pages/invector/[...slug].vue
@@ -1,5 +1,16 @@
 <template>
   <main>
-    <ContentDoc/>
+    <VRow>
+      <VCol>
+        <VBtn @click="() => {router.go(-1)}">記事一覧へ</VBtn>
+      </VCol>
+    </VRow>
+    <VRow>
+      <ContentDoc/>
+    </VRow>
   </main>
 </template>
+
+<script setup>
+const router = useRouter()
+</script>
